01128439f60ef8f7c1255783f70aba765bbdf7f0
rapldez <radoslawzych04@gmail.com>
2026-09-19 20:31:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,8 +15,9 @@ app.listen(PORT, '0.0.0.0', () => {
 });
 
 // Podpięcie osobnego panelu zarządzania i terminala z pliku panel.js
-const panelRouter = require('./panel');
+const panelRouter = require('./panel')(client);
 app.use('/', panelRouter);
+
 
 
 // --- SPRAWDZANIE ZMIENNYCH ŚRODOWISKOWYCH (.env) ---
