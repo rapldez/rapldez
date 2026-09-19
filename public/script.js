c86426d47cdc455fb504c668449a0b78ce651b79
rapldez <radoslawzych04@gmail.com>
2026-09-19 14:54:26 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
         // Zwiększone opóźnienia, żeby tekst pisał się i kasował miękko, bez pośpiechu
-        setTimeout(animateTitle, isDeleting ? 320 : 440);
+        setTimeout(animateTitle, isDeleting ? 440 : 540);
     }
     animateTitle();
 
